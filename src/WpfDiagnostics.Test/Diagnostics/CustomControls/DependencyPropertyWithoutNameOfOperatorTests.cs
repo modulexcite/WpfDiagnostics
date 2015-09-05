9169047290854d01e51357eeb99a642959a8cb3d
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-09-05 23:12:33 +0200

    Initial working version of analyzer.

--- src/WpfDiagnostics.Test/Diagnostics/CustomControls/DependencyPropertyWithoutNameOfOperatorTests.cs
+++ src/WpfDiagnostics.Test/Diagnostics/CustomControls/DependencyPropertyWithoutNameOfOperatorTests.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.CodeAnalysis.Diagnostics;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
 
+using RoslynTester.DiagnosticResults;
 using RoslynTester.Helpers.CSharp;
+
+using WpfDiagnostics.Diagnostics.CustomControls;
 
 using Xunit;
 
@@ -15,6 +20,48 @@ namespace WpfDiagnostics.Test.Diagnostics.CustomControls
             VerifyDiagnostic(test);
         }
 
-        protected override DiagnosticAnalyzer DiagnosticAnalyzer { get; } = new WpfDiagnosticsAnalyzer();
+        // scenario's:
+        // * class without dep prop, should find nothing
+        // * class with depProp, with call to DependencyProperty.Register(), first argument is string
+        // * same as above, first argument is nameof()-expression
+        // * class is not derived from DepObj
+        // * compilation unit is not referencing "Wpf"-assembly
+
+        [Fact]
+        public void RegisterCall_WithoutFirstArgumentNameOfExpression_ShouldChangeIntoNameOf()
+        {
+            var brokenSource = @"
+    using System.Windows;
+
+    public class ClassWithDependencyProperty : DependencyObject
+    {
+        public static readonly DependencyProperty DependencyPropertyNameProperty =
+            DependencyProperty.Register(
+                ""DependencyPropertyName"",
+                typeof(string),
+                typeof(ClassWithDependencyProperty),
+                new PropertyMetadata(default(string)));
+
+        public string DependencyPropertyName
+        {
+            get { return (string)GetValue(DependencyPropertyNameProperty); }
+            set { SetValue(DependencyPropertyNameProperty, value); }
+        }
+    }";
+            var expectedLocation = new LinePosition(8, 17);
+            var expected = new DiagnosticResult
+            {
+                Id = "DependencyPropertyWithoutNameOfOperatorAnalyzer",
+                Message = "Dependency property 'DependencyPropertyName' can use nameof() operator for DependencyProperty.Register() call",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", expectedLocation.Line, expectedLocation.Character)
+                        }
+            };
+            VerifyDiagnostic(brokenSource, expected);
+        }
+
+        protected override DiagnosticAnalyzer DiagnosticAnalyzer { get; } = new DependencyPropertyWithoutNameOfOperatorAnalyzer();
     }
 }
--- src/WpfDiagnostics/Diagnostics/CustomControls/DependencyPropertyWithoutNameOfOperatorAnalyzer.cs
+++ src/WpfDiagnostics/Diagnostics/CustomControls/DependencyPropertyWithoutNameOfOperatorAnalyzer.cs
@@ -1,18 +1,49 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace WpfDiagnostics.Diagnostics.CustomControls
 {
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class DependencyPropertyWithoutNameOfOperatorAnalyzer : DiagnosticAnalyzer
     {
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+        private const string DiagnosticId = nameof(DependencyPropertyWithoutNameOfOperatorAnalyzer);
+        private const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+
+        private static readonly string Category = "Custom Controls";
+        private static readonly string Message = "Dependency property '{0}' can use nameof() operator for DependencyProperty.Register() call";
+        private static readonly string Title = "Use nameof";
+
+        internal static DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
         public override void Initialize(AnalysisContext context)
         {
-            throw new NotImplementedException();
+            context.RegisterSyntaxNodeAction(AnalyzeInvocationExpression, SyntaxKind.InvocationExpression);
+        }
+
+        private void AnalyzeInvocationExpression(SyntaxNodeAnalysisContext context)
+        {
+            var invocationNode = (InvocationExpressionSyntax)context.Node;
+            var memberAccessNode = (MemberAccessExpressionSyntax)invocationNode.Expression;
+            var typeNameNode = (IdentifierNameSyntax) memberAccessNode.Expression;
+            var methodNameNode = (IdentifierNameSyntax) memberAccessNode.Name;
+            if (typeNameNode.Identifier.Text == "DependencyProperty" && methodNameNode.Identifier.Text == "Register")
+            {
+
+                var firstArgumentNode = invocationNode.ArgumentList.Arguments.First();
+                var firstArgumentExpressionNode = firstArgumentNode.Expression;
+                if (firstArgumentExpressionNode.Kind() != SyntaxKind.NameOfKeyword)
+                {
+                    var depPropName = context.SemanticModel.GetConstantValue(firstArgumentExpressionNode).Value;
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, firstArgumentNode.GetLocation(), depPropName));
+                    
+                }
+            }
         }
     }
 }
