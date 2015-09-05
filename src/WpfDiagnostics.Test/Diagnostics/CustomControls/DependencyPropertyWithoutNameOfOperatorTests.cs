625c5f70a78e7ecd5e5e06b2bedd2799ff463e71
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-09-05 21:51:54 +0200

    Add new (skeleton of) new DependencyPropertyWithoutNameOfOperator diagnostic.

---
+++ src/WpfDiagnostics.Test/Diagnostics/CustomControls/DependencyPropertyWithoutNameOfOperatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+using RoslynTester.Helpers.CSharp;
+
+using Xunit;
+
+namespace WpfDiagnostics.Test.Diagnostics.CustomControls
+{
+    public class DependencyPropertyWithoutNameOfOperatorTests : CSharpDiagnosticVerifier
+    {
+        [Fact]
+        public void WithEmptySourceFile_ShouldNotFindAnything()
+        {
+            var test = @"";
+            VerifyDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer DiagnosticAnalyzer { get; } = new WpfDiagnosticsAnalyzer();
+    }
+}
---
+++ src/WpfDiagnostics/Diagnostics/CustomControls/DependencyPropertyWithoutNameOfOperatorAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace WpfDiagnostics.Diagnostics.CustomControls
+{
+    public class DependencyPropertyWithoutNameOfOperatorAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
